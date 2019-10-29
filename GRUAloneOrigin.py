235cf31857d1a85b57d7a754b88cf6d01c37100a
gael <gael.russeil@hotmail.fr>
2019-10-29 15:55:04 +0100

    Prepared terran for testing accuracy

--- GRUAloneOrigin.py
+++ GRUAloneOrigin.py
@@ -8,39 +8,8 @@ from sklearn.model_selection import train_test_split
 
 english_sentences = load_data('data/small_vocab_en')
 french_sentences = load_data('data/small_vocab_fr')
-
 english_words_counter = collections.Counter([word for sentence in english_sentences for word in sentence.split()])
 french_words_counter = collections.Counter([word for sentence in french_sentences for word in sentence.split()])
-
-print('{} English words.'.format(len([word for sentence in english_sentences for word in sentence.split()])))
-print('{} unique English words.'.format(len(english_words_counter)))
-print('10 Most common words in the English dataset:')
-print('"' + '" "'.join(list(zip(*english_words_counter.most_common(10)))[0]) + '"')
-print()
-print('{} French words.'.format(len([word for sentence in french_sentences for word in sentence.split()])))
-print('{} unique French words.'.format(len(french_words_counter)))
-print('10 Most common words in the French dataset:')
-print('"' + '" "'.join(list(zip(*french_words_counter.most_common(10)))[0]) + '"')
-
-# Tokenize Example output
-text_sentences = [
-    'The quick brown fox jumps over the lazy dog .',
-    'By Jove , my quick study of lexicography won a prize .',
-    'This is a short sentence .']
-text_tokenized, text_tokenizer = tokenize(text_sentences)
-print(text_tokenizer.word_index)
-print()
-for sample_i, (sent, token_sent) in enumerate(zip(text_sentences, text_tokenized)):
-    print('Sequence {} in x'.format(sample_i + 1))
-    print('  Input:  {}'.format(sent))
-    print('  Output: {}'.format(token_sent))
-
-# Pad Tokenized output
-test_pad = pad(text_tokenized)
-for sample_i, (token_sent, pad_sent) in enumerate(zip(text_tokenized, test_pad)):
-    print('Sequence {} in x'.format(sample_i + 1))
-    print('  Input:  {}'.format(np.array(token_sent)))
-    print('  Output: {}'.format(pad_sent))
 
 preproc_english_sentences, preproc_french_sentences, english_tokenizer, french_tokenizer = \
     preprocess(english_sentences, french_sentences)
@@ -49,14 +18,14 @@ max_english_sequence_length = preproc_english_sentences.shape[1]
 max_french_sequence_length = preproc_french_sentences.shape[1]
 english_vocab_size = len(english_tokenizer.word_index)
 french_vocab_size = len(french_tokenizer.word_index)
+print('Shape of preproc_english_sentences', preproc_english_sentences.shape)
+print('Shape of preproc_french_sentences', preproc_french_sentences.shape)
 
 print('Data Preprocessed')
 print("Max English sentence length:", max_english_sequence_length)
 print("Max French sentence length:", max_french_sequence_length)
 print("English vocabulary size:", english_vocab_size)
 print("French vocabulary size:", french_vocab_size)
-
-print('`logits_to_text` function loaded.')
 
 # Reshaping the input to work with a basic RNN
 tmp_x = pad(preproc_english_sentences, max_french_sequence_length)
@@ -67,13 +36,13 @@ simple_rnn_model = simple_model(
     tmp_x.shape,
     french_vocab_size)
 
+print('fitting shapes', tmp_x.shape, "(", tmp_x.shape[:1], ")", french_vocab_size, preproc_french_sentences)
+
 print(simple_rnn_model.summary())
 
 simple_rnn_model.fit(tmp_x, preproc_french_sentences, batch_size=1024, epochs=10, validation_split=0.2)
 
 simple_rnn_model.save("models/GRUAlone")
-# Print prediction(s)
-print(logits_to_text(simple_rnn_model.predict(tmp_x[:1])[0], french_tokenizer))
 
 # Print prediction(s)
 print("Prediction:")
@@ -86,6 +55,14 @@ print("\nOriginal text:")
 print(english_sentences[:1])
 
 
+print("Prediction:")
+print(logits_to_text(simple_rnn_model.predict(tmp_x[:1])[0], french_tokenizer))
+
+print("\nCorrect Translation:")
+print(logits_to_text(preproc_french_sentences[:1], french_tokenizer))
+
+print("\nOriginal text:")
+print(logits_to_text(preproc_english_sentences[:1], english_tokenizer))
 
 
 
