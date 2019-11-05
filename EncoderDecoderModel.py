d58fff908c54cd1670fe0b192ecc77e739bca3bd
gael <gael.russeil@hotmail.fr>
2019-11-05 19:34:14 +0100

    Added 3 different models to be tested in parallel

--- EncoderDecoderModel.py
+++ EncoderDecoderModel.py
@@ -1,6 +1,7 @@
 from loader import load_data
 from helper import tokenize, pad, preprocess, sequence_to_text, logits_to_text, text_to_sequence
-from models import encoder_decoder
+from models import encoder_decoderRMSProp, encoder_decoderAdam, encoder_decoderAdamBiggerEmbed, \
+    encoder_decoderAdamOneEmbed
 import collections
 import numpy as np
 import gensim
@@ -59,7 +60,6 @@ print("English vocabulary size:", english_vocab_size)
 print("French vocabulary size:", french_vocab_size)
 
 
-# Reshaping the input to work with a basic RNN
 tmp_x = pad(preproc_english_sentences, max_french_sequence_length)
 
 X_train, X_test, y_train, y_test = train_test_split(english_sentences, french_sentences)
@@ -78,33 +78,19 @@ def generate_batch(X=X_train, y=y_train, batch_size=128):
             decoder_input_data = np.zeros((batch_size, max_french_sequence_length), dtype='float32')
             decoder_target_data = np.zeros((batch_size, max_french_sequence_length, french_vocab_size), dtype='float32')
             for i, (input_text, target_text) in enumerate(zip(X[j:j+batch_size], y[j:j+batch_size])):
-                # print('HEERE')
-                # print(input_text)
-                # print(input_text.shape)
-                # print(target_text)
-                # print(target_text)
-                # print(target_text.split(" "))
-                # print(target_token_index)
                 for t, word in enumerate(input_text.split(" ")):
-                    # print(t, word)
-                    # encoder_input_data[i, t] = word  # encoder input seq
                     try:
-                        encoder_input_data[i, t] = input_token_index[word]  # encoder input seq
+                        encoder_input_data[i, t] = input_token_index[word]
                     except:
                         pass
 
                 for t, word in enumerate(target_text.split(" ")):
                     if t < len(input_text)-1:
-                        # decoder_input_data[i, t] = word  # decoder input seq
                         try:
-                            decoder_input_data[i, t] = target_token_index[word]  # decoder input seq
+                            decoder_input_data[i, t] = target_token_index[word]
                         except:
                             pass
                     if t > 0:
-                        # decoder target sequence (one hot encoded)
-                        # does not include the START_ token
-                        # Offset by one timestep
-                        # decoder_target_data[i, t - 1, word] = 1.
                         try:
                             decoder_target_data[i, t - 1, target_token_index[word]] = 1.
                         except:
@@ -112,7 +98,12 @@ def generate_batch(X=X_train, y=y_train, batch_size=128):
             yield([encoder_input_data, decoder_input_data], decoder_target_data)
 
 
-model, encoder_model, decoder_model = encoder_decoder(english_vocab_size, french_vocab_size)
+# HERE models
+# model, encoder_model, decoder_model = encoder_decoder(english_vocab_size, french_vocab_size)
+model, encoder_model, decoder_model = encoder_decoderRMSProp(english_vocab_size, french_vocab_size)
+# model, encoder_model, decoder_model = encoder_decoderAdam(english_vocab_size, french_vocab_size)
+# model, encoder_model, decoder_model = encoder_decoderAdamBiggerEmbed(english_vocab_size, french_vocab_size)
+# model, encoder_model, decoder_model = encoder_decoderAdamOneEmbed(english_vocab_size, french_vocab_size)
 
 print(model.summary())
 train_samples = len(X_train)
@@ -120,16 +111,17 @@ val_samples = len(X_test)
 batch_size = 128
 epochs = 50
 
-model.fit_generator(generator=generate_batch(X_train, y_train, batch_size=batch_size),
-                    steps_per_epoch=train_samples/batch_size,
-                    epochs=epochs,
-                    validation_data=generate_batch(X_test, y_test, batch_size=batch_size),
-                    validation_steps=val_samples/batch_size)
+# model.fit_generator(generator=generate_batch(X_train, y_train, batch_size=batch_size),
+#                     steps_per_epoch=train_samples/batch_size,
+#                     epochs=epochs,
+#                     validation_data=generate_batch(X_test, y_test, batch_size=batch_size),
+#                     validation_steps=val_samples/batch_size)
 
 # model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.2)
 
-model.save("models/EncoderDecoderModel")
-model.save_weights('EncoderDecoder_weights.h5')
+# model.save("models/EncoderDecoderModel")
+# model.save_weights('models/EncoderDecoder_weights.h5')
+model.load_weights('models/EncoderDecoder_weights.h5')
 
 
 def decode_sequence(input_seq):
@@ -167,43 +159,13 @@ def decode_sequence(input_seq):
 
     return decoded_sentence
 
-train_gen = generate_batch(X_train, y_train, batch_size = 1)
-k=-1
-k+=1
-(input_seq, _), _ = next(train_gen)
-decoded_sentence = decode_sequence(input_seq)
-print('Input English sentence:', X_train[k:k+1].values[0])
-print('Actual Marathi Translation:', y_train[k:k+1].values[0][6:-4])
-print('Predicted Marathi Translation:', decoded_sentence[:-4])
-k+=1
-(input_seq, _), _ = next(train_gen)
-decoded_sentence = decode_sequence(input_seq)
-print('Input English sentence:', X_train[k:k+1].values[0])
-print('Actual Marathi Translation:', y_train[k:k+1].values[0][6:-4])
-print('Predicted Marathi Translation:', decoded_sentence[:-4])
-k+=1
-(input_seq, _), _ = next(train_gen)
-decoded_sentence = decode_sequence(input_seq)
-print('Input English sentence:', X_train[k:k+1].values[0])
-print('Actual Marathi Translation:', y_train[k:k+1].values[0][6:-4])
-print('Predicted Marathi Translation:', decoded_sentence[:-4])
-k+=1
-(input_seq, _), _ = next(train_gen)
-decoded_sentence = decode_sequence(input_seq)
-print('Input English sentence:', X_train[k:k+1].values[0])
-print('Actual Marathi Translation:', y_train[k:k+1].values[0][6:-4])
-print('Predicted Marathi Translation:', decoded_sentence[:-4])
-k+=1
-(input_seq, _), _ = next(train_gen)
-decoded_sentence = decode_sequence(input_seq)
-print('Input English sentence:', X_train[k:k+1].values[0])
-print('Actual Marathi Translation:', y_train[k:k+1].values[0][6:-4])
-print('Predicted Marathi Translation:', decoded_sentence[:-4])
-k+=1
-(input_seq, _), _ = next(train_gen)
-decoded_sentence = decode_sequence(input_seq)
-print('Input English sentence:', X_train[k:k+1].values[0])
-print('Actual Marathi Translation:', y_train[k:k+1].values[0][6:-4])
-print('Predicted Marathi Translation:', decoded_sentence[:-4])
 
-
+train_gen = generate_batch(X_train, y_train, batch_size=1)
+k = -1
+for i in range(10):
+    k += 1
+    (input_seq, _), _ = next(train_gen)
+    decoded_sentence = decode_sequence(input_seq)
+    print('\nInput English sentence:', X_train[i])
+    print('Actual French Translation:', y_train[i])
+    print('Predicted French Translation:', decoded_sentence)
