f13c7b63ffb9640e738a6f1e04b0879578b19159
gael <gael.russeil@hotmail.fr>
2019-11-14 11:30:42 +0100

    Trying model with attention

--- models.py
+++ models.py
@@ -413,7 +413,7 @@ def modelWithAttention(english_vocab_size, french_vocab_size, max_words):
     a = Permute((2, 1))(decoder_inputs)
     a = Dense(max_words, activation='softmax')(a)
     a_probs = Permute((2, 1), name='attention_vec')(a)
-    attention_mul = multiply(decoder_inputs, a_probs)
+    attention_mul = multiply([decoder_inputs, a_probs])
     decoder_outputs, _, _ = decoder_lstm(attention_mul, initial_state=encoder_states)
     decoder_dense = Dense(french_vocab_size, activation='softmax')
     decoder_outputs = decoder_dense(decoder_outputs)
