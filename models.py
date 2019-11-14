d1b6bb3481700ed1db913ef06cdcdfda0e34435d
gael <gael.russeil@hotmail.fr>
2019-11-14 16:31:36 +0100

    Trying model with attention with Bidirectional LSTM, as in the paper

--- models.py
+++ models.py
@@ -445,8 +445,8 @@ def modelWithAttentionBidirectional(english_vocab_size, french_vocab_size, max_w
     enc_emb = Embedding(english_vocab_size, latent_dim, mask_zero=True)(encoder_inputs)
     encoder_lstm = Bidirectional(LSTM(latent_dim * 10, return_state=True))
     encoder_outputs, forward_h, forward_c, backward_h, backward_c = encoder_lstm(enc_emb)
-    state_h = Concatenate()([transpose(forward_h), transpose(backward_h)])
-    state_c = Concatenate()([transpose(forward_c), transpose(backward_c)])
+    state_h = Concatenate()(forward_h, backward_h)
+    state_c = Concatenate()(forward_c, backward_c)
     encoder_states = [state_h, state_c]
 
     decoder_inputs = Input(shape=(max_words, 1,))
